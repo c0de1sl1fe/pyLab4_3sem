2ff5c4f5c6ebc3cf5d6109a522adb76d6e066875
_Yury_ <90544915+c0de1sl1fe@users.noreply.github.com>
2022-11-24 20:04:23 +0400

    add parrt creating 3 new cols

--- utils.py
+++ utils.py
@@ -1,8 +1,17 @@
 
-
+import cv2
 import numpy as np
 import pandas as pd
 
+def img_specs0(path):
+    img = cv2.imread(path)
+    return img.shape[0]
+def img_specs1(path):
+    img = cv2.imread(path)
+    return img.shape[1]
+def img_specs2(path):
+    img = cv2.imread(path)
+    return img.shape[2]
 
 def createDF():
     path1 = "dataset/zebra/zebra_annotation.csv"
@@ -15,3 +24,8 @@ def createDF():
     df = pd.concat(dfs)
     df.drop(1, axis=1, inplace=True)
     df.rename(columns={0:'path', 2:'class'}, inplace=True)
+
+    df['height'] = df['path'].apply(img_specs0)
+    df['width'] = df['path'].apply(img_specs1)
+    df['channels'] = df['path'].apply(img_specs2)
+
